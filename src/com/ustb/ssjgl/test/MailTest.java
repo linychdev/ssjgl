cd6cbfa9c80bc535b218396af26e3067b20b3c13
linychdev <505494784@qq.com>
2018-03-28 01:09:36 +0800

    增加发送邮件工具类

---
+++ src/com/ustb/ssjgl/test/MailTest.java
@@ -0,0 +1,21 @@
+package com.ustb.ssjgl.test;
+
+import java.io.UnsupportedEncodingException;
+
+import javax.mail.MessagingException;
+
+import com.ustb.ssjgl.mail.util.MailUtil;
+
+public class MailTest {
+	public static void main(String[] args) {
+		try {
+			MailUtil.sendMail("403215845@qq.com", "test", "美女你好！");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
