b9bbec94730cbc906bd1623ad225896900603043
王萍 <wangping-1@thunisoft.com>
2018-05-03 22:10:02 +0800

    增加前端页面

---
+++ web/js/index.js
@@ -0,0 +1,35 @@
+$(function(){
+ 
+   // 开始写 jQuery 代码...
+  // var docs = $("div.elements");
+   $("div.elements").on("mouseover",function(){
+
+   	   var htmlstr="";
+   	   var oxidhtml="";
+       var oxid = $(this).find("span.oxidation").text();
+       if(oxid!=null&&oxid!=""&&oxid!=undefined){
+          var oxidArr= oxid.split(",");
+          for(i=0;i<oxidArr.length;i++){
+             oxidhtml+="<div class='oxidation-big'>"+oxidArr[i]+"</div>";
+          }
+       }
+         
+
+    htmlstr+="<div class ='elements-big'>"+oxidhtml+
+		"<div class = 'electron-big'>"+$(this).find("span.electron").text()+"</div>"+
+		"<div class = 'eleativity-big'>"+$(this).find("span.eleativity").text()+"</div>"+
+		"<div class = 'ionic-big'>"+$(this).find("span.ionic").text()+"</div>"+
+		"<div class = 'boiling-big'>"+$(this).find("span.boiling").text()+"</div>"+
+		"<div class = 'melting-big'>"+$(this).find("span.melting").text()+"</div>"+
+		"<div class = 'relative-big'>"+$(this).find("span.relative").text()+"</div>"+
+		"<div class = 'atomic-big'>"+$(this).find("div.atomic").text()+"</div>"+
+		"<div class = 'symbol-big'>"+$(this).find("div.symbol").text()+"</div>"+
+		"<div class = 'element-name-big'>"+$(this).find("div.element").text()+"</div>"+
+	    "</div>";
+	$(".show_element_detail").width(200);
+	$(".show_element_detail").height(200);
+	$(".show_element_detail").html(htmlstr);
+   });
+
+  
+});
