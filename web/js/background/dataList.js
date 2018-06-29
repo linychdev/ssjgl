9d2bc1d71368a1b2055bdd4c7802591b134b2e23
linych <linych@thunisoft.com>
2018-06-29 21:26:17 +0800

    增加注释

--- web/js/background/dataList.js
+++ web/js/background/dataList.js
@@ -4,11 +4,11 @@ $(function () {
   var laypage = layui.laypage, layer = layui.layer;
   laypage.render({
        elem: 'paging-div'
-       ,count: record //从服务器获取
+       ,count: record //从服务器获取到的数据总量
        ,layout: ['count', 'prev', 'page', 'next', 'limit', 'refresh', 'skip']
           ,curr : pageIndex
           ,limit : pageSize
-          ,limits : [15, 30, 50, 100]
+          ,limits : [15, 30, 50, 100]//可选的每页最大展示行数
        ,jump: function(obj){
         if(obj.curr != pageIndex){
            $(location).attr("href", "dataList?pageIndex="+obj.curr + "&pageSize="+obj.limit);
